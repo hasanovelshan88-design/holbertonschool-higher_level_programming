2c9adf545f40db61de43f485cffe4b158887296d
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-03-01 19:21:30 +0000

    fix bool check

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,6 @@
-#!/usr/bin/python3
-"""Module for BaseGeometry class."""
-
-
-class BaseGeometry:
-    """A class with geometry base methods."""
-
-    def area(self):
-        """Raises an Exception."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
+def integer_validator(self, name, value):
         """Validates value."""
-        if type(value) is not int:
+        if type(value) is not int or type(value) is bool:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
