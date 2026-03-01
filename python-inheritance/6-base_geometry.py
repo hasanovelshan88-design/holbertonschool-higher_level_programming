81716dbf1ae7fd9835286b3b4d79f155f544a1fb
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-03-01 18:31:19 +0000

    Add task 6: Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Calculate area - not yet implemented
+
+        Raises:
+            Exception: always, with message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
