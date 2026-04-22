641d812d7beaaae6c2e695bbff22a0589d0e09a1
Your Name <you@example.com>
2026-04-22 20:14:42 +0000

    Add task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with filter."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of a Student."""
+        if isinstance(attrs, list) and all(
+                isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items()
+                    if k in attrs}
+        return self.__dict__
