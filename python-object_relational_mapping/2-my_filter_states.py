f39ecf4d1d56d00e677f24de038e1183f3c31141
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-04-23 00:33:14 +0000

    Fix 2-my_filter_states.py - fully correct version

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""Lists all states matching the given name"""
+"""Lists all states matching the given name using format"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-                .format(sys.argv[4]))
-    [print(row) for row in cur.fetchall()]
-    cur.close()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            sys.argv[4]))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
     db.close()
