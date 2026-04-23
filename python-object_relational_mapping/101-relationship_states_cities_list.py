813310195d966662fb5272eb5bced47a0f1e1383
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-04-23 00:39:19 +0000

    Fix 101-relationship_states_cities_list.py - proper clean version

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-"""Lists all State objects and corresponding City objects"""
+"""
+Script that lists all State objects and corresponding City objects
+contained in the database hbtn_0e_101_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+def list_states_and_cities(username, password, database):
+    """Lists all states and their corresponding cities"""
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, database
+        ),
+        pool_pre_ping=True
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
+
     states = session.query(State).order_by(State.id).all()
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
-        for city in sorted(state.cities, key=lambda c: c.id):
+        for city in sorted(state.cities, key=lambda city: city.id):
             print("\t{}: {}".format(city.id, city.name))
+
     session.close()
+
+
+if __name__ == "__main__":
+    list_states_and_cities(sys.argv[1], sys.argv[2], sys.argv[3])
