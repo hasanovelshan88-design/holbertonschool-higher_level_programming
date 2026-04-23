351a97e1975f2fe1918922caf8993b1978ce40e9
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-04-23 00:15:16 +0000

    Add 2-my_filter_states.py - filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Lists all states matching the given name"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(sys.argv[4]))
+    [print(row) for row in cur.fetchall()]
+    cur.close()
+    db.close()
