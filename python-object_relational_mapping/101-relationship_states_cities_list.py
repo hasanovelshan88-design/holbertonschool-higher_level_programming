7460adfd5e7f3431a67980530169ac544dd1a5fe
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-04-23 00:40:36 +0000

    Fix 101-relationship_states_cities_list.py - proper version

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -9,10 +9,11 @@ from relationship_city import City
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    session = sessionmaker(bind=engine)()
-    [print("{}: {}\n{}".format(
-        s.id, s.name,
-        "\n".join("\t{}: {}".format(c.id, c.name)
-                  for c in sorted(s.cities, key=lambda c: c.id)))
-     for s in session.query(State).order_by(State.id)]
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in sorted(state.cities, key=lambda c: c.id):
+            print("\t{}: {}".format(city.id, city.name))
     session.close()
