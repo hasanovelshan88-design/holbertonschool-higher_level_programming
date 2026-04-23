98374fd5cec9f1f64007640b314bf6c381145066
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-04-23 01:59:28 +0000

    Fix 6-my_calculator.py

--- python-import_modules/6-my_calculator.py
+++ python-import_modules/6-my_calculator.py
@@ -4,7 +4,7 @@ import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py ")
+        print("Usage: ./6-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
     operator = sys.argv[2]
