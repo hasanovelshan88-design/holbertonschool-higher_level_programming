d6c60bf9ab909949f96fd9ec1b42edce92e3eaa6
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-04-23 01:45:59 +0000

    Add 100-my_calculator.py

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Calculator program using functions from calculator_1.py"""
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    operators = {'+': add, '-': sub, '*': mul, '/': div}
+
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, operators[operator](a, b)))
