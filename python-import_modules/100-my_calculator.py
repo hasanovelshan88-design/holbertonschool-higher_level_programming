552b50737e48ec63445ece40cd6e1ed9ad986585
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-04-23 02:06:38 +0000

    Fix 100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
 """Calculator program using functions from calculator_1.py"""
 import sys
-import calculator_1
-
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./6-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-
-    operators = {
-        '+': calculator_1.add,
-        '-': calculator_1.sub,
-        '*': calculator_1.mul,
-        '/': calculator_1.div
-    }
-
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-
     print("{} {} {} = {}".format(a, operator, b, operators[operator](a, b)))
