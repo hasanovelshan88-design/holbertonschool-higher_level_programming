660f55ab994a812e2d24c970ed2d17cb1ce53298
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-04-23 02:03:34 +0000

    Fix 6-my_calculator.py

--- python-import_modules/6-my_calculator.py
+++ python-import_modules/6-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-    operators = {'+': add, '-': sub, '*': mul, '/': div}
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
