60949d4e61eccd5b9445dd4593acd7acf3167b85
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-07-23 19:02:32 +0000

    Fix final query with joinedload for task 16

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,17 +10,15 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
-    )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).outerjoin(State.cities).\
-        options(joinedload(State.cities)).\
-        order_by(State.id, City.id).all()
+    states = session.query(State).options(
+        joinedload(State.cities)
+    ).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
