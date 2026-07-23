99616e8ec38fc2d65eaacdc9eafea1e074b7cb21
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-07-23 19:08:08 +0000

    Fix final sorting and single query for task 16

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
+        for city in sorted(state.cities, key=lambda c: c.id):
             print("\t{}: {}".format(city.id, city.name))
 
     session.close()
