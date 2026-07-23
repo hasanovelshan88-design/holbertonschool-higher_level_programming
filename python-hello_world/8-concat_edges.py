d8bc82afb968ba8e142df3a19af42d5d447850ab
hasanovelshan88-design <hasanovelshan88@gmail.com>
2026-07-23 18:08:49 +0000

    Fix task 6 no variables no string literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming with Python"
-str2 = "Python"
-str3 = "programming"
-print(str1[:16] + str3 + str1[27:])
+print("object-oriented" + " " + "programming" + " with " + "Python")
